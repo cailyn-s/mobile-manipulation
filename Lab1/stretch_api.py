46a6e23b2a4337816d0c2c16add7de27c8574a8c
ccsmith03 <cailynclaires@gmail.com>
2026-01-21 16:12:05 -0500

    Add stretch api commands for lab1

---
+++ Lab1/stretch_api.py
@@ -0,0 +1,65 @@
+import stretch_body.robot
+import numpy as np
+
+robot = stretch_body.robot.Robot()
+robot.startup()
+
+robot.stow()
+robot.push_command()
+robot.wait_command()
+
+# Extend arm and raise lift
+robot.arm.move_to(0.52)
+robot.lift.move_to(1.1)
+robot.push_command()
+robot.wait_command()
+
+# Rotate wrist in each direction, one at a time
+robot.end_of_arm.move_to('wrist_yaw', np.radians(30))
+robot.push_command()
+robot.wait_command()
+
+robot.end_of_arm.move_to('wrist_pitch', np.radians(30))
+robot.push_command()
+robot.wait_command()
+
+robot.end_of_arm.move_to('wrist_roll', np.radians(30))
+robot.push_command()
+robot.wait_command()
+
+# Open the gripper and close it
+robot.end_of_arm.move_to('stretch_gripper', 100) # open
+robot.push_command()
+robot.wait_command()
+
+robot.end_of_arm.move_to('stretch_gripper', -100) # closed
+robot.push_command()
+robot.wait_command()
+
+# Rotate the head
+robot.head.move_by('head_pan', np.radians(45)) # Move head pan
+robot.head.move_by('head_tilt', np.radians(45)) # Move head tilt
+robot.push_command()
+robot.wait_command()
+
+# Move back to stow
+robot.stow()
+robot.push_command()
+robot.wait_command()
+
+# Move forward 0.5 meters
+robot.base.translate_by(0.5)
+robot.push_command()
+robot.wait_command()
+
+# Rotate 180 degrees
+robot.base.rotate_by(np.radians(180))
+robot.push_command()
+robot.wait_command()
+
+# Move forward 0.5 meters again
+robot.base.translate_by(0.5)
+robot.push_command()
+robot.wait_command()
+
+robot.stop()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mobile-manipulation
+`ros2 launch stretch_core stretch_driver.launch.py`
+
+`ros2 run rviz2 rviz2 -d `ros2 pkg prefix --share
+stretch_calibration`/rviz/stretch_simple_test.rviz`
