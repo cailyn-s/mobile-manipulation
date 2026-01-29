41df4473138da9ba3000f54004714057f7836ab2
ccsmith03 <cailynclaires@gmail.com>
2026-01-29 10:32:02 -0500

    Working ros code

--- Lab1/ros_api.py
+++ Lab1/ros_api.py
@@ -10,22 +10,24 @@ node = hm.HelloNode.quick_create('temp')
 node.stow_the_robot()
 
 # Extend arm and raise lift
-node.move_to_pose({'joint_arm': 0.52}, blocking=False)
-node.move_to_pose({'joint_lift': 1.1}, blocking=False)
-time.sleep(3)
+node.move_to_pose({'joint_arm': 0.52, 'joint_lift': 1.1}, blocking=True)
 
 # Rotate wrist in each direction, one at a time
-node.move_to_pose({'joint_wrist_yaw': np.radians(30)}, blocking=True)
-node.move_to_pose({'joint_wrist_pitch': np.radians(30)}, blocking=True)
-node.move_to_pose({'joint_wrist_roll': np.radians(30)}, blocking=True)
+node.move_to_pose({'joint_wrist_roll': np.radians(-20)}, blocking=True)
+time.sleep(0.1)
+node.move_to_pose({'joint_wrist_roll': np.radians(20)}, blocking=True)
+time.sleep(0.1)
+node.move_to_pose({'joint_wrist_pitch': np.radians(15)}, blocking=True)
+node.move_to_pose({'joint_wrist_yaw': np.radians(15)}, blocking=True)
 
 # Open the gripper and close it
-node.move_to_pose({'joint_gripper_finger_left': 1.3}, blocking=True)
-node.move_to_pose({'joint_gripper_finger_left': 0}, blocking=True)
+node.move_to_pose({'joint_gripper_finger_left': 1.0}, blocking=True)
+node.move_to_pose({'joint_gripper_finger_left': 0.0}, blocking=True)
 
 # Rotate the head
-node.move_to_pose({'joint_head_pan': np.radians(45)}, blocking=True)
-node.move_to_pose({'joint_head_tilt': np.radians(45)}, blocking=True)
+node.move_to_pose({'joint_head_pan': np.radians(20)}, blocking=True)
+time.sleep(0.5)
+node.move_to_pose({'joint_head_tilt': np.radians(-30)}, blocking=True)
 
 # Move back to stow
 node.stow_the_robot()
@@ -38,3 +40,6 @@ node.move_to_pose({'rotate_mobile_base': np.radians(180)}, blocking=True)
 
 # Move forward 0.5 meters again
 node.move_to_pose({'translate_mobile_base': 0.5}, blocking=True)
+
+node.destroy_node()
+rclpy.shutdown()
